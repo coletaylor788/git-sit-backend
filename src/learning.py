7f0009b479291bf9d5a81eeb68fb416a907626af
Cole Taylor <coletaylor788@gmail.com>
2018-04-11 10:12:18 -0400

    Updating training

--- src/learning.py
+++ src/learning.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pylab as plt
 from matplotlib.pylab import rcParams
+
 from sklearn.neural_network import MLPRegressor
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold, cross_val_score
 from sklearn.externals import joblib
+
 import sys
 
 
@@ -34,8 +36,8 @@ def read_data(file):
 # P has been converted to -1 (Klaus)
 def experiment_train_independent_model():
     x, y = read_data(IND_DATA_FILE)
-    #x = [[1,2],[3,4],[1,3]]
-    #y = [1,4,5]
+    #x = [[1,2],[3,4],[1,3],[1,2],[3,4],[1,3],[1,2],[3,4],[1,3],[5,6]]
+    #y = [1,4,5,1,4,5,1,4,5,6]
     np_x = np.array(x)
     np_y = np.array(y)
     np_x = np_x.astype(np.dtype('float'))
@@ -43,14 +45,29 @@ def experiment_train_independent_model():
 
     clf = MLPRegressor(verbose=True)
     print("Training")
-    scores = cross_val_score(clf, np_x, np_y, cv=5, )
-    print(scores)
-    
-    print("Training")
-    clf.fit(np_x, np_y)
-    joblib.dump(clf, 'independent_model_temp.pkl')
+    k_fold = KFold(n_splits=5)
+    scores = []
+    i = 0
+    for train_indices, test_indices in k_fold.split(np_x):
+        model = clf.fit(np_x[train_indices], np_y[train_indices])
+        score = model.score(np_x[test_indices], np_y[test_indices])
+        scores.append(score)
 
-#experiment_train_independent_model()
+        joblib.dump(model, 'ind_model_' + str(i) + '.pkl')
+
+        i += 1
+
+    with open('ind_model_cv_results.txt', 'w') as outfile:
+        outfile.write(str(scores))
+
+    #scores = cross_val_score(clf, np_x, np_y, cv=5, )
+    #print(scores)
+    
+    #print("Training")
+    #clf.fit(np_x, np_y)
+    #joblib.dump(clf, 'independent_model_temp.pkl')
+
+experiment_train_independent_model()
 
 ######################################
 # Prediction
